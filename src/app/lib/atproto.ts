4e4a365491285bc6bdeafba3a48cf35054284e8c
JavaG23 <geof.hoffman@gmail.com>
2025-03-14 17:56:16 -0400

    client-metadata.json route added

---
+++ src/app/lib/atproto.ts
@@ -0,0 +1,28 @@
+
+import {
+
+  OAuthClientMetadataInput,
+} from '@atproto/oauth-client-browser'
+
+
+export function blueskyClientMetadata(): OAuthClientMetadataInput {
+  const baseUrl: string = process.env.NEXT_PUBLIC_URL as string
+
+  return {
+    client_name: 'atproto oAuth template',
+    client_id: `${baseUrl}/client-metadata.json`,
+    client_uri: `${baseUrl}`,
+    redirect_uris: [`${baseUrl}/oauth/callback`],
+    policy_uri: `${baseUrl}/policy`,
+    tos_uri: `${baseUrl}/tos`,
+    scope: 'atproto transition:generic',
+    grant_types: ['authorization_code', 'refresh_token'],
+    response_types: ['code'],
+    application_type: 'web',
+    token_endpoint_auth_method: 'none',
+    dpop_bound_access_tokens: true,
+  }
+}
+
+
+
